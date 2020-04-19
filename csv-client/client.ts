0b80de879d294e61ba9de63207bb3eab4f120f11
Charles Feval <chfeval@microsoft.com>
2020-04-19 12:57:34 -0700

    Client start

---
+++ csv-client/client.ts
@@ -0,0 +1,11 @@
+// const fs = require("fs");
+// const parse = require('csv-parse/lib/sync')
+// const lowCleric = require("../src/app")
+
+// const file = process.argv[2];
+// const content = fs.readFileSync(file);
+// const tasks = parse(content, { columns: true, skip_empty_lines: true })
+
+
+
+// console.log(tasks)
